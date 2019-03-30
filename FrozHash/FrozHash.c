f8dac9546889d6fdeebe93c8f946e48e507f2d56
Frozander <Frozander@users.noreply.github.com>
2019-03-30 20:17:05 +0300

    >Initial commit for FrozHash.c

---
+++ FrozHash/FrozHash.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    if (argc < 3) 
+    {
+
+        printf("\nUsage: ./FrozHash <inputString> <Key | Optional>");
+        return -1;
+
+    }
+    
+    return 0;
+}
