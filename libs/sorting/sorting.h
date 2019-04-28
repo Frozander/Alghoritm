f40cfc0096474e7d4d91e30904fad7ac23442a03
Frozander <Frozander@users.noreply.github.com>
2019-04-28 21:20:36 +0300

    >Added sorting Library utility functions

---
+++ libs/sorting/sorting.h
@@ -0,0 +1,10 @@
+#define ARRAYLENGTH 1000000
+
+#if ARRAYLENGTH > 2147483647
+    #define ARRAYLENGTH 2147483647
+    #endif
+
+void swap(int *a, int *b);
+void printArray(int targetArray[], int k);
+void populateArray(int targetArray[],int k);
+int isSorted(int targetArray[], int k);
